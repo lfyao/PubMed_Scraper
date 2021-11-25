7cd0b4f2ab44718d06999e2d44e4a500474358e0
Yao Lean Franzl Lim <leanfranzl-y@kogecha.naist.jp>
2021-11-25 18:27:29 +0900

    Editted script to have main folder and txt file to specifies dates of the main folder

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
 import os
+
 # import re
 from datetime import datetime
 from pathlib import Path
@@ -15,8 +16,9 @@ import shutil
 
 from functions import *
 from specifications import *
+
 # %%
-os.makedirs(output_dir, exist_ok = True)
+os.makedirs(output_dir, exist_ok=True)
 
 start_time = datetime.now()
 
@@ -47,46 +49,52 @@ start_time = datetime.now()
 # Scrape data
 save_to_csv_pmid_of_extracted_abstracts(output_dir, save_file_name)
 
-_, N = search(query=query,
-              entrez_email=entrez_email,
-              retstart=retstart,
-              sort=sort,
-              mindate=duration[0],
-              maxdate=duration[1],
-              retmax=retmax)
+_, N = search(
+    query=query,
+    entrez_email=entrez_email,
+    retstart=retstart,
+    sort=sort,
+    mindate=duration[0],
+    maxdate=duration[1],
+    retmax=retmax,
+)
 
 N = int(N)
 _, counter = list_from_csv(save_file_name)
 
-print(f'Extracting {N - counter:,} abstracts.')
+print(f"Extracting {N - counter:,} abstracts.")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     while counter < N:
         # extracted_list, retstart = list_from_csv(save_file_name)
         extracted_list, _ = list_from_csv(save_file_name)
         retstart += retmax
 
         # pmid_list, pmid_count = search(query=query,retstart=retstart, retmax=retmax)
-        
+
         # retmax = min(retmax, int(pmid_count))
 
-        pmid_list, pmid_count = search(query=query,
-                                       entrez_email=entrez_email,
-                                       retstart=retstart,
-                                       sort=sort,
-                                       mindate=duration[0],
-                                       maxdate=duration[1],
-                                       retmax=retmax)
+        pmid_list, pmid_count = search(
+            query=query,
+            entrez_email=entrez_email,
+            retstart=retstart,
+            sort=sort,
+            mindate=duration[0],
+            maxdate=duration[1],
+            retmax=retmax,
+        )
 
         if len([x for x in pmid_list if x not in extracted_list]) < retmax:
             # if there's too much overlap, widen the search space
-            pmid_list, pmid_count = search(query=query,
-                                       entrez_email=entrez_email,
-                                       retstart=retstart,
-                                       sort=sort,
-                                       mindate=duration[0],
-                                       maxdate=duration[1],
-                                       retmax=2*retmax)
+            pmid_list, pmid_count = search(
+                query=query,
+                entrez_email=entrez_email,
+                retstart=retstart,
+                sort=sort,
+                mindate=duration[0],
+                maxdate=duration[1],
+                retmax=2 * retmax,
+            )
 
         n_extracted = extract_abstract(
             pmid_list=pmid_list,
@@ -94,23 +102,24 @@ if __name__ == '__main__':
             save_file_name=save_file_name,
             retstart=retstart,
             pmid_count=pmid_count,
-            output_dir=output_dir)
+            output_dir=output_dir,
+        )
 
         counter += n_extracted
 
-        print(f'Extracted {counter:,} out of {N:,}.')
+        print(f"Extracted {counter:,} out of {N:,}.")
 
 # %%
 # Summarize xml files to one csv file
 from xml_to_csv import *
 
 # Zip output xml to zip file
-zip_files(output_dir, zip_name=f'xml_{folder_name}')
+zip_files(output_dir, zip_name=f"xml_{folder_name}")
 shutil.rmtree(str(output_dir))
 
 # %%
 end_time = datetime.now()
-print(f'Started: {start_time}')
-print(f'Finished: {end_time}')
-print('Duration: {}'.format(end_time - start_time))
+print(f"Started: {start_time}")
+print(f"Finished: {end_time}")
+print("Duration: {}".format(end_time - start_time))
 # %%
--- specifications.py
+++ specifications.py
@@ -13,21 +13,31 @@ from bs4 import BeautifulSoup
 main = True
 retstart = 0
 sort = "pub+date"
-duration = ('2016/01/01', '2021/10/31')
+duration = ("2016/01/01", "2021/10/31")
 # duration = ('2021/07/07', '2021/07/07')
 retmax = 1000
-query = 'case report'
-
-folder_name = f"pubmed (duration = {duration[0].replace('/', '-')} to {duration[1].replace('/', '-')}, query = '{query}')"
-if main:
-    folder_name = f"pubmed (duration = {duration[0].replace('/', '-')} to _, query = '{query}')"
+query = "case report"
 
 home_dir = Path("/home/is/leanfranzl-y/Python/")
 # home_dir = Path('/Users/leanfranzl-y/Documents/Python/')
 
-output_dir = home_dir / f'Data/Output/PubMed_Scraper/{folder_name}/'.replace('//', '/')
+folder_name = f"pubmed (duration = {duration[0].replace('/', '-')} to {duration[1].replace('/', '-')}, query = '{query}')"
 
-save_file_name = str(home_dir) + f'/Data/Output/PubMed_Scraper/pubmed_id_{folder_name}.csv'
-summary_csv_file_name = str(home_dir) + f'/Data/Output/PubMed_Scraper/summary_{folder_name}.csv'
+if main:
+    folder_name = f"pubmed (duration = main, query = '{query}')"
 
-entrez_email = 'yao.lean_franzl_lim.yg3@is.naist.jp'
+    with open(str(home_dir) + f"/Data/Output/PubMed_Scraper/main_specs.txt", "w") as f:
+        f.write(f"{duration[0]} {duration[1]}")
+
+output_dir = home_dir / f"Data/Output/PubMed_Scraper/{folder_name}/".replace("//", "/")
+
+save_file_name = (
+    str(home_dir) + f"/Data/Output/PubMed_Scraper/pubmed_id_{folder_name}.csv"
+)
+summary_csv_file_name = (
+    str(home_dir) + f"/Data/Output/PubMed_Scraper/summary_{folder_name}.csv"
+)
+
+entrez_email = "yao.lean_franzl_lim.yg3@is.naist.jp"
+
+# %%
